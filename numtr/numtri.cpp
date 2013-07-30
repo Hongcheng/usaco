e4e8308320a0d88d080b43a8e38effe43afda463
rso <xdzhouhongcheng@163.com>
2013-07-30 11:02:13 +0800

    7.30 add numtri

---
+++ numtr/numtri.cpp
@@ -0,0 +1,50 @@
+/*ID:xdzhouh3
+LANG:C++
+TASK:numtri
+*/
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <cstring>
+using namespace std;
+ifstream fin("numtri.in");
+ofstream fout("numtri.out");
+// int cmp(const void* a,const void*b)
+// {
+//     return (*(worktime *)a).t1>(*(worktime *)b).t1?1:-1;
+// }
+//     qsort(wt,N,sizeof(wt[0]),cmp);
+int R;
+int num[1000][1000];
+int main()
+{
+    cin >> R;
+    int i,j,maxnum=0;
+    for(i =0;i<R;i++)
+    {
+        for(j = 0;j<=i;j++)
+        {
+            cin >> num[i][j];
+        }
+    }
+    for(i = 1;i<R;i++)
+    {
+        for(j = 0;j<=i;j++)
+        {
+            if(j == 0)
+                num[i][0] = num[i-1][0]+num[i][0];
+            else if(j == i)
+                num[i][j] = num[i-1][j]+num[i][j];
+            else
+                num[i][j] = max(num[i-1][j-1],num[i-1][j]) + num[i][j];
+        }
+    }
+    for(i = 0;i<R;i++)
+    {
+        cout << num[R-1][i] << " ";
+        if(num[R-1][i] > maxnum)
+            maxnum = num[R-1][i];
+    }cout << endl;
+    cout << maxnum;
+    return 0;
+}
